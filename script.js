9f54d780e54d3fdc1676ed7928fbe34a6fa61495
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2024-02-06 17:43:51 +0600

    ✨feat: implemeted book animation

---
+++ script.js
@@ -0,0 +1,50 @@
+let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+document.querySelectorAll(".page").forEach((page, index) => {
+    page.innerHTML = `Page ${index + 1}`;
+});
+
+const startScrollPosition =
+    window.scrollY || document.documentElement.scrollTop;
+
+const totalHeight = document.querySelector(".scroll-container").scrollHeight;
+const scrollReact = document
+    .querySelector(".scroll-container")
+    .getBoundingClientRect();
+const angelPerPx = 180 / (totalHeight - window.innerHeight);
+
+window.addEventListener(
+    "scroll",
+    function handleScroll() {
+        const scrollTopPosition =
+            window.scrollY || document.documentElement.scrollTop;
+
+        document.querySelectorAll(".page").forEach((page, index) => {
+            let rotatePercentage =
+                (scrollTopPosition - startScrollPosition) *
+                angelPerPx *
+                (index + 1);
+
+            rotatePercentage = Math.min(rotatePercentage, 180);
+
+            if (scrollTopPosition > lastScrollTop) {
+                page.setAttribute(
+                    "style",
+                    `transform: rotateY(${rotatePercentage}deg);z-index: -${
+                        index + 1
+                    }
+                    `
+                );
+            } else if (scrollTopPosition < lastScrollTop) {
+                page.setAttribute(
+                    "style",
+                    `transform: rotateY(${rotatePercentage}deg);
+                    z-index: -${index + 1}`
+                );
+            }
+        });
+
+        lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
+    },
+    false
+);
